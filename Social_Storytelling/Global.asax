72fdcd5a9ccb622ee160b803769e75f2d9353be6
zachcannon <zach.cannon10@gmail.com>
2014-02-17 21:46:27 -0600

    Initial commit of blank solution projects generated by Visual Stuido

---
+++ Social_Storytelling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Social_Storytelling.MvcApplication" Language="C#" %>
---
+++ Social_Storytelling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social_Storytelling.Startup))]
+namespace Social_Storytelling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
