7a27f8a705d950889efd9d1e45420f60f1a97fa5
zachcannon <zach.cannon10@gmail.com>
2014-04-15 10:42:41 -0500

    Added new files for admin pages

---
+++ SocialStorytelling/Controllers/AdminViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialStorytelling.Controllers
+{
+    public class AdminViewController : Controller
+    {
+        //
+        // GET: /AdminView/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
